7b3bc3af089c02e98b1896245620af03f3f5c716
lilantii <103457340+lilantii@users.noreply.github.com>
2026-05-01 22:31:44 +0200

    add translation and header

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Overview } from './features/overview/overview';
+import { Cv } from './features/cv/cv';
+import { Skills } from './features/skills/skills';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "overview", component: Overview},
+    {path: "cv", component: Cv},
+    {path: "skills", component: Skills},
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,26 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TranslateDirective, TranslateService} from '@ngx-translate/core'
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TranslateDirective],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
+  standalone: true,
 })
 export class App {
   protected readonly title = signal('portfolio');
+
+  private translate = inject(TranslateService);
+
+    constructor() {
+    this.translate.setDefaultLang('de');
+    this.translate.use('de');
+  }
+
+  switchLanguage(event: Event) {
+    const lang = (event.target as HTMLSelectElement).value;
+    this.translate.use(lang);
+  }
 }
